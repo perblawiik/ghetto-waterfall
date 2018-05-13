5c62a21ee0c3c2075dd82ef2863082733627ac14
Jonas Långdahl <jonazz.97@gmail.com>
2018-05-13 11:50:30 +0200

    Added music to new scene

---
+++ UnityProject/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+    public AudioClip[] clips;
+    public AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        clips = Resources.LoadAll<AudioClip>("Music");
+        audioSource = GetComponent<AudioSource>();
+        audioSource.loop = false;
+		
+	}
+
+    void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.clip = clips[Random.Range(0, clips.Length)];
+            audioSource.Play();
+        }
+    }
+}
