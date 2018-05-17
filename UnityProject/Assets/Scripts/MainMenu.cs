1c28b174a725965aaed89a3193432e2ffb7efe6c
Per Blåwiik <perj.sg@gmail.com>
2018-05-17 10:30:10 +0200

    Revert "Shopping cart sound and stuff"
    This reverts commit 43e6584110d371f956157d224384684470b55f42.

--- UnityProject/Assets/Scripts/MainMenu.cs
+++ UnityProject/Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour {
 
 	public void PlayGame ()
     {
-        SceneManager.LoadScene("GameAlpha");
+        SceneManager.LoadScene("PerGameDemo");
     }
 
     public void QuitGame()
